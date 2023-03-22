3a3f6aa9776e0e32fc852acd9e0436993e4de409
elal20 <128519759+elal20@users.noreply.github.com>
2023-03-22 13:19:13 +0400

    Add files via upload

--- diabetes_pred.py.py
+++ diabetes_pred.py.py
@@ -84,3 +84,22 @@ if (selected == 'Diabetes Prediction'):
     st.success(diab_diagnosis)
     
     
+    # Email Notification 
+    import smtplib, ssl
+    port = 587  # For starttls
+    smtp_server = "smtp.gmail.com"
+    sender_email = "elias.alam20@gmail.com"
+    receiver_email = Email
+    password = "eabr30102000"
+    message = """\
+    Subject: Hi there
+    Im sending an email through python code."""
+    context = ssl.create_default_context()
+    with smtplib.SMTP(smtp_server, port) as server:
+         server.starttls(context=context)
+    server.ehlo() 
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, message) 
+
+
+    
